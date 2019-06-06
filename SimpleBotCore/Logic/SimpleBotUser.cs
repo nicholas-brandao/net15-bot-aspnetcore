d4bf493955afea2f569b48a7586365c7eba81581
Nicholas Brandao <nicholas.brandao1@gmail.com>
2019-06-05 22:23:10 -0300

    Adicionado Desafio 1 e Desafio 2

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +10,53 @@ namespace SimpleBotCore.Logic
 {
     public class SimpleBotUser
     {
+
+        private static MongoClient conn = new MongoClient("mongodb://localhost:27017");
+
+        private static IMongoDatabase db = conn.GetDatabase("15net");
+
         public string Reply(SimpleMessage message)
         {
-            return $"{message.User} disse '{message.Text}'";
+
+            Salvar(message);
+
+            return $"{message.User} disse '{message.Text}' ({RetornaNumeroMensagensEnviadas(message.Id)} mensagens enviadas)";
         }
 
+
+        void Salvar(SimpleMessage message)
+        {
+            #region Desafio 1
+            var col = db.GetCollection<BsonDocument>("colMensagens");
+
+            var doc = new BsonDocument() {
+                                           { "Id", message.Id },
+                                           { "Usuario", message.User },
+                                           { "Mensagem", message.Text }
+                                         };
+            col.InsertOne(doc);
+            #endregion
+
+            #region Desafio 2
+            var colContMensagensEnviadas = db.GetCollection<BsonDocument>("colMensagensEnviadas");
+
+            var cont = colContMensagensEnviadas.Find(new BsonDocument() {
+                                                                          { "Id", message.Id }
+                                                                         }).ToList();
+
+            var docContMensagem = new BsonDocument() {
+                                           { "Id", message.Id },
+                                           { "Cont", cont.Count+1 }
+                                         };
+            colContMensagensEnviadas.InsertOne(docContMensagem);
+            #endregion
+        }
+
+        private string RetornaNumeroMensagensEnviadas(string idUsuario)
+        {
+            return db.GetCollection<BsonDocument>("colMensagensEnviadas").Find(new BsonDocument() {
+                                                                          { "Id", idUsuario }
+                                                                         }).ToList().Count().ToString();
+        }
     }
 }
